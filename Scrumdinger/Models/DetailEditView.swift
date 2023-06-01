c8f0594c2e11495dd457108e99350c7b67647f19
AntoninaNayden <naydena>
2023-06-01 12:00:00 +0300

    Artificial contribution 1

---
+++ Scrumdinger/Models/DetailEditView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Antonina on 5.10.24.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailEditView()
+}
